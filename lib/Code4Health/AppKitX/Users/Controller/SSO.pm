f7f1daf583fb61a92ffc6b3e72fed125d2fbf692
Colin Newell <colin@opusvl.com>
2015-06-15 11:34:53 +0100

    Validate the vanilla request

--- lib/Code4Health/AppKitX/Users/Controller/SSO.pm
+++ lib/Code4Health/AppKitX/Users/Controller/SSO.pm
@@ -3,7 +3,7 @@ package Code4Health::AppKitX::Users::Controller::SSO;
 use Moose;
 use namespace::autoclean;
 use Cpanel::JSON::XS;
-use Code4Health::AppKitX::Users::Vanilla qw/vanilla_signature/;
+use Code4Health::AppKitX::Users::Vanilla qw/vanilla_signature vanilla_request_validate/;
 BEGIN { extends 'Catalyst::Controller'; };
 with 'OpusVL::AppKit::RolesFor::Controller::GUI';
 
@@ -14,16 +14,14 @@ sub authenticate
 {
     my ($self, $c) = @_;
     $c->stash->{no_wrapper} = 1;
-    # FIXME: send back with content type of json
     my $client_id = $c->req->param('client_id');
     my $callback = $c->req->param('callback');
     my $timestamp = $c->req->param('timestamp');
-    # FIXME: verify the timestamp.
     my $signature = $c->req->param('signature');
-    $self->_bad_request($c, 'Invalid callback function') unless $callback =~ /^\w[\w_]*$/;
+    $self->_bad_request($c, 'invalid_request', 'Invalid callback function') unless $callback =~ /^\w[\w_]*$/;
     $c->stash->{callback_func} = $callback;
-    $self->_bad_request($c, 'Specify client id') unless $client_id;
-    $self->_bad_request($c, 'Incorrect client id') unless $client_id eq $c->config->{vanilla_client_id};
+    $self->_bad_request($c, 'invalid_request', 'The client_id parameter is missing.') unless $client_id;
+    $self->_bad_request($c, 'invalid_client', 'Unknown client.') unless $client_id eq $c->config->{vanilla_client_id};
 
     my $secret = $c->config->{vanilla_secret};
 
@@ -33,6 +31,16 @@ sub authenticate
         if($timestamp)
         {
             # FIXME: add roles
+            $self->_bad_request($c, 'invalid_request', 'Missing signature parameter') unless $signature;
+            $self->_bad_request($c, 'access_denied', 'Signature invalid') unless vanilla_request_validate($timestamp, $secret, $signature);
+
+            my $current_time = time;
+            my $timestamp_diff_allowed = $c->config->{vanilla_timestamp_margin} || 60*30;
+            if($current_time - $timestamp_diff_allowed > $timestamp)
+            {
+                $self->_bad_request($c, 'invalid_request', 'The timestamp is invalid.');
+            }
+
             $c->stash->{data} = {
                 uniqueid => $c->user->id,
                 email => $c->user->email_address,
@@ -59,9 +67,9 @@ sub authenticate
 
 sub _bad_request
 {
-    my ($self, $c, $problem) = @_;
+    my ($self, $c, $problem, $message) = @_;
     $c->res->status(400);
-    $c->res->body(encode_json({ error => $problem }));
+    $c->res->body(encode_json({ error => $problem, message => $message }));
     $c->response->headers->header('Content-Type', 'application/javascript');
     $c->detach;
 }
--- lib/Code4Health/AppKitX/Users/Vanilla.pm
+++ lib/Code4Health/AppKitX/Users/Vanilla.pm
@@ -2,9 +2,10 @@ package Code4Health::AppKitX::Users::Vanilla;
 
 use URI::Escape;
 use Digest::SHA1 qw/sha1_hex/;
+use String::Compare::ConstantTime;
 
 our @ISA = qw/Exporter/;
-our @EXPORT_OK = qw/vanilla_signature/;
+our @EXPORT_OK = qw/vanilla_signature vanilla_request_validate/;
 
 sub vanilla_signature
 {
@@ -18,6 +19,13 @@ sub vanilla_signature
     my $signature = sha1_hex($combined . $secret);
 
     return $signature;
+}
+
+sub vanilla_request_validate
+{
+    my ($timestamp, $secret, $signature) = @_;
+    my $expected = sha1_hex($timestamp . $secret);
+    return String::Compare::ConstantTime::equals($signature, $expected);
 }
 
 1;
