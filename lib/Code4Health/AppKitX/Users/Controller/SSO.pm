8579360a0db5bd5c75a663623a53fd7181f58854
Colin Newell <colin@opusvl.com>
2015-06-15 10:55:59 +0100

    Fixed various bits

--- lib/Code4Health/AppKitX/Users/Controller/SSO.pm
+++ lib/Code4Health/AppKitX/Users/Controller/SSO.pm
@@ -15,16 +15,15 @@ sub authenticate
     my ($self, $c) = @_;
     $c->stash->{no_wrapper} = 1;
     # FIXME: send back with content type of json
-    $c->response->headers->header('Content-Type', 'application/javascript');
     my $client_id = $c->req->param('client_id');
     my $callback = $c->req->param('callback');
     my $timestamp = $c->req->param('timestamp');
     # FIXME: verify the timestamp.
     my $signature = $c->req->param('signature');
-    $self->_bad_request('Invalid callback function') unless $callback =~ /^\w[\w_]*$/;
+    $self->_bad_request($c, 'Invalid callback function') unless $callback =~ /^\w[\w_]*$/;
     $c->stash->{callback_func} = $callback;
-    $self->_bad_request('Specify client id') unless $client_id;
-    $self->_bad_request('Incorrect client id') unless $client_id eq $c->config->{vanilla_client_id};
+    $self->_bad_request($c, 'Specify client id') unless $client_id;
+    $self->_bad_request($c, 'Incorrect client id') unless $client_id eq $c->config->{vanilla_client_id};
 
     my $secret = $c->config->{vanilla_secret};
 
@@ -36,7 +35,7 @@ sub authenticate
             # FIXME: add roles
             $c->stash->{data} = {
                 uniqueid => $c->user->id,
-                email => $c->user->email,
+                email => $c->user->email_address,
                 name => $c->user->username, 
                 photourl => '',
                 client_id => $client_id,
@@ -55,13 +54,15 @@ sub authenticate
     {
         $c->stash->{data} = {name => '', photourl => ''};
     }
+    $c->response->headers->header('Content-Type', 'application/javascript');
 }
 
 sub _bad_request
 {
     my ($self, $c, $problem) = @_;
     $c->res->status(400);
-    $c->res->content(encode_json({ error => $problem }));
+    $c->res->body(encode_json({ error => $problem }));
+    $c->response->headers->header('Content-Type', 'application/javascript');
     $c->detach;
 }
 
