7a8a384ba10928804a5e649e07f36a13f2cb6bf5
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2015-07-01 16:19:02 +0100

    Sort secondary org list

--- lib/Code4Health/AppKitX/Users/Controller/Users.pm
+++ lib/Code4Health/AppKitX/Users/Controller/Users.pm
@@ -198,7 +198,11 @@ sub profile
         $c->flash->{status_msg} = "Profile saved";
     }
 
-
+    $c->stash->{secondary_organisations} = [ 
+        $c->user
+            ->secondary_organisations
+            ->search({}, { order_by => "name" })
+    ];
     $c->stash->{template} = 'modules/users/organisations_form.tt';
     $c->stash->{no_wrapper} = 1;
     $c->forward($c->view('AppKitTT'));
